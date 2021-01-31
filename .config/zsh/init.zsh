65b2e1eb9f6e8c1c56c3db1dcacea92657e4922d
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-01-31 18:27:04 +0100

    bootstrap v0.1a

--- .config/zsh/init.zsh
+++ .config/zsh/init.zsh
@@ -6,15 +6,16 @@ source "$ZDOTDIR/.zgenom/zgenom.zsh"
 if ! zgenom saved; then
 
     # bulk load
-    zgenom loadall <<EOPLUGINS
-        zsh-users/zsh-autosuggestions
-        kazhala/dotbare
-        lukechilds/zsh-nvm
-        b4b4r07/enhancd
-        unixorn/autoupdate-zgen
-        wfxr/forgit
-        zdharma/fast-syntax-highlighting
-EOPLUGINS
+    zgenom load zsh-users/zsh-autosuggestions
+    zgenom load kazhala/dotbare
+    zgenom load lukechilds/zsh-nvm
+    zgenom load b4b4r07/enhancd
+    zgenom load unixorn/autoupdate-zgen
+    zgenom load wfxr/forgit
+    zgenom load zdharma/fast-syntax-highlighting
+
+    zgenom load junegunn/fzf shell/completion.zsh 
+    zgenom load junegunn/fzf shell/key-bindings.zsh 
 
     # completions
     zgenom load zsh-users/zsh-completions src
@@ -24,6 +25,10 @@ EOPLUGINS
 
     # save all to init script
     zgenom save
+
+    zgenom compile "$HOME/.zshenv"
+    zgenom compile "$XDG_CONFIG_HOME/zsh"
+    zgenom compile "$XDG_DATA_HOME/zsh/generated"
 fi
 
 # quick modules
@@ -55,7 +60,7 @@ unset comp_files
 # - Then fall back to case-insensitive.
 # - Accept abbreviations after . or _ or - (ie. f.b -> foo.bar).
 # - Substring complete (ie. bar -> foobar).
-zstyle ':completion:*' matcher-list '' '+m:{[:lower:]}={[:upper:]}' '+m:{[:upper:]}={[:lower:]}' '+m:{_-}={-_}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+# zstyle ':completion:*' matcher-list '' '+m:{[:lower:]}={[:upper:]}' '+m:{[:upper:]}={[:lower:]}' '+m:{_-}={-_}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
 # Colorize completions using default `ls` colors.
 zstyle ':completion:*' list-colors ''
--- .zshenv
+++ .zshenv
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-export XDG_CONFIG_HOME="$HOME/.config"
-export ZDOTDIR="$HOME/.config/zsh"
+XDG_CONFIG_HOME="$HOME/.config"
+ZDOTDIR="$HOME/.config/zsh"
 
 source "$ZDOTDIR/.zshenv"
