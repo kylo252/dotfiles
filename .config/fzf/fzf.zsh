7ce263f4d57bf2f35b0fc28ab730b06f8cb4edd1
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-02-06 12:38:02 +0100

    fzf: added config files

---
+++ .config/fzf/fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/khgh/.local/share/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/khgh/.local/share/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/khgh/.local/share/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/khgh/.local/share/fzf/shell/key-bindings.zsh"
