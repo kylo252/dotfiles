79b85a8ed9c169bc10a746d53ef39518c13b6303
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-02-15 17:57:09 +0100

    init: clean-up

--- .config/zsh/init.zsh
+++ .config/zsh/init.zsh
@@ -1,4 +1,18 @@
 #!/usr/bin/env zsh
+# shellcheck disable
+
+autoload -U compinit
+# Initialize the completion system with a cache time of 24 hours.
+typeset -g zcompdump="$HOME/.local/share/zsh/zcompdump"
+typeset -g comp_files=($zcompdump(Nm-24))
+if (( $#comp_files )) {
+  compinit -i -C -d $zcompdump
+} else {
+  compinit -i -d $zcompdump
+}
+
+unset zcompdump
+unset comp_files
 
 source "$XDG_DATA_HOME/zgenom/zgenom.zsh"
 
@@ -37,32 +51,8 @@ autoload -Uz kp
 _dotbare_completion_cmd
 # _dotbare_completion_git
 
-autoload -U compinit
-
-# Initialize the completion system with a cache time of 24 hours.
-typeset -g zcompdump="$HOME/.local/share/zsh/zcompdump"
-typeset -g comp_files=($zcompdump(Nm-24))
-
-if (( $#comp_files )) {
-  compinit -i -C -d $zcompdump
-} else {
-  compinit -i -d $zcompdump
-}
-
-unset zcompdump
-unset comp_files
-# Make completion:
-# - Try exact (case-sensitive) match first.
-# - Then fall back to case-insensitive.
-# - Accept abbreviations after . or _ or - (ie. f.b -> foo.bar).
-# - Substring complete (ie. bar -> foobar).
-# zstyle ':completion:*' matcher-list '' '+m:{[:lower:]}={[:upper:]}' '+m:{[:upper:]}={[:lower:]}' '+m:{_-}={-_}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-
 # Colorize completions using default `ls` colors.
 zstyle ':completion:*' list-colors ''
-
-# Allow completion of ..<Tab> to ../ and beyond.
-zstyle -e ':completion:*' special-dirs '[[ $PREFIX = (../)#(..) ]] && reply=(..)'
 
 # $CDPATH is overpowered (can allow us to jump to 100s of directories) so tends
 # to dominate completion; exclude path-directories from the tag-order so that
@@ -71,7 +61,6 @@ zstyle ':completion:*:complete:(cd|pushd):*' tag-order 'local-directories named-
 
 # Categorize completion suggestions with headings:
 zstyle ':completion:*' group-name ''
-zstyle ':completion:*:descriptions' format %F{default}%B%{$__WINCENT[ITALIC_ON]%}--- %d ---%{$__WINCENT[ITALIC_OFF]%}%b%f
 
 # Enable keyboard navigation of completions in menu
 # (not just tab/shift-tab but cursor keys as well):
