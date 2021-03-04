161f1af5b85b82f731cbc79fb6198c41340b9064
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-03-04 13:22:04 +0100

    p10k: fixed merge issue

--- .config/zsh/.p10k.default.zsh
+++ .config/zsh/.p10k.default.zsh
@@ -46,7 +46,6 @@
     status                  # exit code of the last command
     command_execution_time  # duration of the last command
     background_jobs         # presence of background jobs
-<<<<<<< HEAD
     direnv                  # direnv status (https://direnv.net/)
     asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
     virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
@@ -62,25 +61,6 @@
     # proxy                 # system-wide http/https/ftp proxy
     # battery               # internal battery
     # wifi                  # wifi speed
-=======
-    # direnv                # direnv status (https://direnv.net/)
-    # asdf                  # asdf version manager (https://github.com/asdf-vm/asdf)
-    # virtualenv            # python virtual environment (https://docs.python.org/3/library/venv.html)
-    # anaconda              # conda environment (https://conda.io/)
-    pyenv                   # python environment (https://github.com/pyenv/pyenv)
-    nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
-    nvm                     # node.js version from nvm (https://github.com/nvm-sh/nvm)
-    # package               # name@version from package.json (https://docs.npmjs.com/files/package.json)
-    context                 # user@hostname
-    # nordvpn               # nordvpn connection status, linux only (https://nordvpn.com/)
-    ranger                  # ranger shell (https://github.com/ranger/ranger)
-    # nnn                   # nnn shell (https://github.com/jarun/nnn)
-    # vim_shell             # vim shell indicator (:sh)
-    # load                    # CPU load
-    # disk_usage            # disk usage
-    # ram                     # free RAM
-    time                  # current time
->>>>>>> ede0920 (p10k: updated config)
     # example               # example user-defined segment (see prompt_example function below)
   )
 
