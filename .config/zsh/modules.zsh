36062ac6d342d20d49282668559f04892df87ff7
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-03-28 12:08:46 +0200

    zshrc: cleanup

--- .config/zsh/modules.zsh
+++ .config/zsh/modules.zsh
@@ -1,6 +1,12 @@
 #!/usr/bin/env zsh
 # shellcheck disable
 
+# custom modules
+fpath=("$ZDOTDIR/modules" "${fpath[@]}")
+
+# load modules
+autoload -Uz kp
+
 autoload -U compinit
 
 # Initialize the completion system with a cache time of 24 hours.
@@ -14,42 +20,7 @@ if (( $#comp_files )) {
 
 unset zcompdump
 unset comp_files
-#
-source "$XDG_DATA_HOME/zgenom/zgenom.zsh"
 
-# if the init script doesn't exist
-if ! zgenom saved; then
-
-    zgenom load zsh-users/zsh-autosuggestions
-    # zgenom load lukechilds/zsh-nvm
-    zgenom load wfxr/forgit
-
-    # completions
-    zgenom load zsh-users/zsh-completions src
-
-    # prompt
-    zgenom load romkatv/powerlevel10k powerlevel10k
-
-    zgenom load b4b4r07/enhancd
-    zgenom load kazhala/dotbare
-    zgenom load unixorn/autoupdate-zgen
-    zgenom load zdharma/fast-syntax-highlighting
-    zgenom load StackExchange/blackbox
-
-    # save all to init script
-    zgenom save
-fi
-
-# quick modules
-fpath=("$ZDOTDIR/modules" "${fpath[@]}")
-
-# load modules
-autoload -Uz kp
-
-# enable completion for dotbare
-# _dotbare_completion_cmd
-# _dotbare_completion_git
-#
 # Colorize completions using default `ls` colors.
 zstyle ':completion:*' list-colors ''
 
@@ -64,3 +35,5 @@ zstyle ':completion:*' group-name ''
 # Enable keyboard navigation of completions in menu
 # (not just tab/shift-tab but cursor keys as well):
 zstyle ':completion:*' menu select
+
+
