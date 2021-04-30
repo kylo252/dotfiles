7d9956067244f0113fd510fec149a9fe2d5b64a8
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-04-30 15:18:30 +0200

    nvim: added missing file

---
+++ .config/nvim/lua/config/formatter.lua
@@ -0,0 +1,26 @@
+require('formatter').setup({
+  logging = false,
+  filetype = {
+    sh = {
+      -- shfmt
+      function()
+        return {
+          exe = "shfmt",
+          args = {"-i 2 -ci"},
+          stdin = true
+        }
+      end
+    },
+    lua = {
+        -- luafmt
+        function()
+          return {
+            exe = "luafmt",
+            args = {"--indent-count", 2, "--stdin"},
+            stdin = true
+          }
+        end
+      }
+  }
+})
+
