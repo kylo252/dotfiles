1f80f6ee31d74eb794a407b067a9cd80ee7e2fa9
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-07-29 16:37:11 +0200

    no need to require which-key outside of setup

--- .config/nvim/lua/core/whichkey.lua
+++ .config/nvim/lua/core/whichkey.lua
@@ -1,44 +1,42 @@
--- Disable some operators
-local presets = require "which-key.plugins.presets"
-presets.operators["v"] = nil
-presets.operators["d"] = nil
-
 -- these are mostly the default values
-require("which-key").setup {
-  plugins = {
-    marks = true, -- shows a list of your marks on ' and `
-    registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
-    -- the presets plugin, adds help for a bunch of default keybindings in Neovim
-    -- No actual key bindings are created
-    presets = {
-      operators = true, -- adds help for operators like d, y, ...
-      motions = true, -- adds help for motions
-      text_objects = true, -- help for text objects triggered after entering an operator
-      windows = true, -- default bindings on <c-w>
-      nav = true, -- misc bindings to work with windows
-      z = true, -- bindings for folds, spelling and others prefixed with z
-      g = true, -- bindings for prefixed with g
-    },
-  },
-  icons = {
-    breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-    separator = "➜", -- symbol used between a key and it's label
-    group = "+", -- symbol prepended to a group
-  },
-  window = {
-    border = "single", -- none, single, double, shadow
-    position = "bottom", -- bottom, top
-    margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-    padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-  },
-  layout = {
-    height = { min = 4, max = 25 }, -- min and max height of the columns
-    width = { min = 20, max = 50 }, -- min and max width of the columns
-    spacing = 3, -- spacing between columns
-  },
-  hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-  show_help = true, -- show help message on the command line when the popup is visible
-}
+require("which-key").setup({
+	plugins = {
+		marks = true, -- shows a list of your marks on ' and `
+		registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+		-- the presets plugin, adds help for a bunch of default keybindings in Neovim
+		-- No actual key bindings are created
+		presets = { -- adds help for operators like d, y, ...
+			operators = {
+        v = nil,
+        d = nil
+      },
+			motions = true, -- adds help for motions
+			text_objects = true, -- help for text objects triggered after entering an operator
+			windows = true, -- default bindings on <c-w>
+			nav = true, -- misc bindings to work with windows
+			z = true, -- bindings for folds, spelling and others prefixed with z
+			g = true, -- bindings for prefixed with g
+		},
+	},
+	icons = {
+		breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+		separator = "➜", -- symbol used between a key and it's label
+		group = "+", -- symbol prepended to a group
+	},
+	window = {
+		border = "single", -- none, single, double, shadow
+		position = "bottom", -- bottom, top
+		margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+		padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+	},
+	layout = {
+		height = { min = 4, max = 25 }, -- min and max height of the columns
+		width = { min = 20, max = 50 }, -- min and max width of the columns
+		spacing = 3, -- spacing between columns
+	},
+	hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
+	show_help = true, -- show help message on the command line when the popup is visible
+})
 
 -- Set leader
 vim.api.nvim_set_keymap("n", "<Space>", "<NOP>", { noremap = true, silent = true })
@@ -54,19 +52,19 @@ local global_opts = {
 }
 
 local global_mappings = {
-  Q = { "<cmd>BufferClose<cr>", "close buffer" },
-  ["<tab>"] = { "<cmd>BufferNext<cr>", "jump to next buffer" },
-  ["<s-tab>"] = { "<cmd>BufferPrevious<cr>", "jump to prev buffer" },
-  ["<C-p>"] = { "<cmd>Telescope find_files<CR>", "Open project files" },
-  ["<M-f>"] = { "<cmd>Telescope live_grep<CR>", "Grep project files" },
-  ["<S-k>"] = { "<cmd>lua vim.lsp.buf.signature_help({focusable = false})<CR>", "Signature Help" },
-  g = {
-    -- LSP
-    d = { "<cmd>lua vim.lsp.buf.definition()<CR>", "Go to definition" },
-    D = { "<cmd>lua vim.lsp.buf.declaration()<CR>", "Go to decleration" },
-    r = { "<cmd>lua vim.lsp.buf.references()<CR>", "Go to references" },
-    i = { "<cmd>lua vim.lsp.buf.implementation()<CR>", "Go to implementation" },
-  },
+	Q = { "<cmd>BufferClose<cr>", "close buffer" },
+	-- ["<tab>"] = { "<cmd>BufferNext<cr>", "jump to next buffer" },
+	-- ["<s-tab>"] = { "<cmd>BufferPrevious<cr>", "jump to prev buffer" },
+	["<C-p>"] = { "<cmd>Telescope find_files<CR>", "Open project files" },
+	["<M-f>"] = { "<cmd>Telescope live_grep<CR>", "Grep project files" },
+	["<S-k>"] = { "<cmd>lua vim.lsp.buf.signature_help({focusable = false})<CR>", "Signature Help" },
+	g = {
+		-- LSP
+		d = { "<cmd>lua vim.lsp.buf.definition()<CR>", "Go to definition" },
+		D = { "<cmd>lua vim.lsp.buf.declaration()<CR>", "Go to decleration" },
+		r = { "<cmd>lua vim.lsp.buf.references()<CR>", "Go to references" },
+		i = { "<cmd>lua vim.lsp.buf.implementation()<CR>", "Go to implementation" },
+	},
 }
 
 wk.register(global_mappings, global_opts)
