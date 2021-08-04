433f653bdcab2a1bce124ec2fb5558207cf2c911
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-08-04 15:42:38 +0200

    lvim: updates

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -8,6 +8,17 @@ lvim.colorscheme = "tokyonight"
 lvim.lsp.diagnostics.virtual_text = false
 lvim.lsp.default_keybinds = true
 lvim.lsp.smart_cwd = true
+lvim.builtin.rooter.active = false
+
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.side = "left"
+lvim.builtin.nvimtree.show_icons.git = 1
+lvim.builtin.nvimtree.hide_dotfiles = 0
+lvim.builtin.nvimtree.ignore = { "" }
+lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.hide_dotfiles = false
+lvim.builtin.rooter.active = false
+
 -- keymappings
 lvim.leader = "space"
 lvim.keys.insert_mode["<C-s>"] = "<cmd>w<cr>"
@@ -15,9 +26,12 @@ lvim.keys.normal_mode["<C-s>"] = "<cmd>w<cr>"
 lvim.keys.normal_mode["<S-q>"] = ":BufferWipeout<cr>"
 lvim.keys.normal_mode["gL"] = ":BufferNext<cr>"
 lvim.keys.normal_mode["gH"] = ":BufferPrev<cr>"
+lvim.keys.normal_mode["<space><space>"] = "<cmd>BufferNext<cr>"
 lvim.keys.normal_mode["<leader>Lc"] = ":e ~/.config/lvim/config.lua<cr>"
 lvim.keys.normal_mode["<leader>Li"] =
 	"<cmd>lua package.loaded['core.info']=nil; require('core.info').toggle_display()<cr>"
+vim.api.nvim_set_keymap("i", "<c-y>", "compe#confirm({ 'keys': '<c-y>', 'select': v:true })", { expr = true })
+
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
@@ -32,61 +46,45 @@ lvim.builtin.dashboard.custom_section = {
 	m = { description = { "  Marks              " }, command = "Telescope marks" },
 }
 
-lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 1
-lvim.builtin.nvimtree.hide_dotfiles = 0
-lvim.builtin.nvimtree.ignore = { "" }
-
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {}
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
--- generic LSP settings
-lvim.lang.sh.formatters = { { exe = "shfmt" } }
-lvim.lang.sh.linters = { { exe = "shellcheck" } }
-lvim.lang.lua.linters = { { exe = "luacheck" } }
-lvim.lang.lua.formatters = { { exe = "stylua" } }
-lvim.lang.typescript.formatters = { { exe = "prettier" } }
-lvim.lang.typescript.linter = { { exe = "eslint" } }
-
-lvim.builtin.rooter.active = false
 
 -- Additional Plugins
 lvim.plugins = {
-	{ "folke/tokyonight.nvim" },
-	{
-		"ray-x/lsp_signature.nvim",
-		config = function()
-			require("lsp_signature").on_attach()
-		end,
-		event = "InsertEnter",
-	},
-	{
-		"folke/trouble.nvim",
-		config = [[require('user.core.trouble')]],
-		-- event = "BufWinEnter"
-	},
-	{ "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
-	{ "tpope/vim-fugitive" },
 	{
 		"ThePrimeagen/harpoon",
 		config = function()
 			require("harpoon").setup()
 		end,
 	},
+  { "folke/tokyonight.nvim" },
+  {
+    "ray-x/lsp_signature.nvim",
+    config = function()
+      require("lsp_signature").on_attach()
+    end,
+    event = "InsertEnter",
+  },
+  {
+    "folke/trouble.nvim",
+    config = [[require('user.core.trouble')]],
+    -- event = "BufWinEnter"
+  },
+  { "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
 }
 
 lvim.builtin.which_key.mappings["lh"] = {
-	function()
-		local buf = vim.api.nvim_get_current_buf()
-		local ft = vim.api.nvim_buf_get_option(buf, "filetype")
-		if ft == "c" or ft == "cpp" then
-			vim.cmd([[ClangdSwitchSourceHeader]])
-		end
-	end,
-	"Go To Header",
+  function()
+    local buf = vim.api.nvim_get_current_buf()
+    local ft = vim.api.nvim_buf_get_option(buf, "filetype")
+    if ft == "c" or ft == "cpp" then
+      vim.cmd [[ClangdSwitchSourceHeader]]
+    end
+  end,
+  "Go To Header",
 }
 
 lvim.builtin.which_key.mappings["H"] = {
@@ -96,17 +94,38 @@ lvim.builtin.which_key.mappings["H"] = {
 }
 
 require("user.core.telescope")
---
--- local custom_mappings = require("user.core.whichkey").mappings
---
--- local og_nvim_tree_cmd = lvim.builtin.which_key.mappings["e"]
---
--- lvim.builtin.which_key.mappings = custom_mappings
--- lvim.builtin.which_key.mappings["e"] = og_nvim_tree_cmd
---
--- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- lvim.autocommands.custom_groups = {
---   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
--- }
+-- set a formatter if you want to override the default lsp one (if it exists)
+local prettier_config = {
+  exe = "prettier",
+}
 
--- Additional Leader bindings for WhichKey
+local eslint_config = {
+  exe = "eslint",
+}
+
+local eslint_d_config = {
+  exe = "eslint_d",
+}
+
+
+-- generic LSP settings
+lvim.lang.sh.formatters = { { exe = "shfmt" } }
+lvim.lang.sh.linters = { { exe = "shellcheck" } }
+
+lvim.lang.python.linters = { { exe = "flake8" } }
+lvim.lang.python.formatters = { { exe = "black" } }
+
+lvim.lang.lua.formatters = { { exe = "stylua" } }
+lvim.lang.lua.linters = { { exe = "luacheck" } }
+
+-- set an additional linter
+lvim.lang.javascript.formatters = { prettier_config }
+lvim.lang.javascript.linters = { eslint_d_config }
+
+lvim.lang.typescript.formatters = { prettier_config }
+lvim.lang.typescript.linters = { eslint_d_config }
+lvim.lang.typescriptreact.formatters = { eslint_d_config }
+lvim.lang.typescriptreact.linters = { eslint_d_config }
+
+-- require "user.keymappings"
+-- require "user.core.telescope"
