3565b33af44562ca8a334abe039377045b8dc12b
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-08-04 15:36:26 +0200

    lvim: updates

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,18 +1,23 @@
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 
 -- general
-lvim.debug = false
+lvim.debug = true
 lvim.format_on_save = true
 lvim.lint_on_save = true
 lvim.colorscheme = "tokyonight"
 lvim.lsp.diagnostics.virtual_text = false
 lvim.lsp.default_keybinds = true
+lvim.lsp.smart_cwd = true
 -- keymappings
 lvim.leader = "space"
-lvim.keys.normal_mode["<S-l>"] = ""
-lvim.keys.normal_mode["<S-h>"] = ""
+lvim.keys.insert_mode["<C-s>"] = "<cmd>w<cr>"
+lvim.keys.normal_mode["<C-s>"] = "<cmd>w<cr>"
+lvim.keys.normal_mode["<S-q>"] = ":BufferWipeout<cr>"
 lvim.keys.normal_mode["gL"] = ":BufferNext<cr>"
 lvim.keys.normal_mode["gH"] = ":BufferPrev<cr>"
+lvim.keys.normal_mode["<leader>Lc"] = ":e ~/.config/lvim/config.lua<cr>"
+lvim.keys.normal_mode["<leader>Li"] =
+	"<cmd>lua package.loaded['core.info']=nil; require('core.info').toggle_display()<cr>"
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
@@ -21,7 +26,7 @@ lvim.builtin.dashboard.custom_section = {
 	a = { description = { "  Recently Used Files" }, command = "Telescope oldfiles" },
 	b = { description = { "  Find File          " }, command = "Telescope find_files" },
 	c = { description = { "  Plugins            " }, command = ":edit ~/.config/nvim/lua/plugins.lua" },
-	s = { description = { "  Settings           " }, command = ":edit ~/.config/nvim/lua/settings.lua" },
+	s = { description = { "  Settings           " }, command = ":edit ~/.config/lvim/config.lua" },
 	d = { description = { "  Find Word          " }, command = "Telescope live_grep" },
 	n = { description = { "  Load Last Session  " }, command = "SessionLoad" },
 	m = { description = { "  Marks              " }, command = "Telescope marks" },
@@ -29,7 +34,9 @@ lvim.builtin.dashboard.custom_section = {
 
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.show_icons.git = 1
+lvim.builtin.nvimtree.hide_dotfiles = 0
+lvim.builtin.nvimtree.ignore = { "" }
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {}
@@ -37,15 +44,14 @@ lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
 -- generic LSP settings
--- you can set a custom on_attach function that will be used for all the language servers
--- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
+lvim.lang.sh.formatters = { { exe = "shfmt" } }
+lvim.lang.sh.linters = { { exe = "shellcheck" } }
+lvim.lang.lua.linters = { { exe = "luacheck" } }
+lvim.lang.lua.formatters = { { exe = "stylua" } }
+lvim.lang.typescript.formatters = { { exe = "prettier" } }
+lvim.lang.typescript.linter = { { exe = "eslint" } }
+
+lvim.builtin.rooter.active = false
 
 -- Additional Plugins
 lvim.plugins = {
@@ -63,6 +69,13 @@ lvim.plugins = {
 		-- event = "BufWinEnter"
 	},
 	{ "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
+	{ "tpope/vim-fugitive" },
+	{
+		"ThePrimeagen/harpoon",
+		config = function()
+			require("harpoon").setup()
+		end,
+	},
 }
 
 lvim.builtin.which_key.mappings["lh"] = {
@@ -76,8 +89,13 @@ lvim.builtin.which_key.mappings["lh"] = {
 	"Go To Header",
 }
 
--- require "user.keymappings"
--- require "user.core.telescope"
+lvim.builtin.which_key.mappings["H"] = {
+	name = "+harpoon",
+	a = { "<cmd>lua require('harpoon.mark').add_file()<cr>", "Add file" },
+	t = { "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", "Toggle menu" },
+}
+
+require("user.core.telescope")
 --
 -- local custom_mappings = require("user.core.whichkey").mappings
 --
