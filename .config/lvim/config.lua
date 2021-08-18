8234f33d26e23cb2cb6155b670c8da095640348a
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-08-18 14:29:59 +0200

    lvim: updates

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,5 +1,5 @@
 -- general
-
+lvim.builtin.telescope.active = true
 lvim.log.level = "error"
 lvim.format_on_save = true
 lvim.lint_on_save = true
@@ -8,22 +8,20 @@ lvim.lsp.diagnostics.virtual_text = false
 lvim.lsp.default_keybinds = true
 lvim.lsp.smart_cwd = true
 lvim.lsp.diagnostics.update_in_insert = true
-lvim.builtin.rooter.active = false
-
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.active = true
 lvim.builtin.compe.active = true
 lvim.builtin.nvimtree.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 1
 lvim.builtin.nvimtree.hide_dotfiles = 0
-lvim.builtin.nvimtree.ignore = { "" }
+-- lvim.builtin.nvimtree.ignore = { "" }
 lvim.builtin.nvimtree.show_icons.git = 0
 lvim.builtin.nvimtree.hide_dotfiles = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {}
 
-require "user.keymappings"
+-- require("user.keymappings")
 -- require "scratch"
 
 -- keymappings
@@ -60,6 +58,13 @@ lvim.builtin.which_key.mappings["f"] = {
   p = { "<cmd>Telescope git_files<CR>", "Find Project Files" },
   r = { "<cmd>Telescope oldfiles<CR>", "Find Recenct Files" },
 }
+lvim.builtin.which_key.mappings["S"] = {
+  name = "+sessions",
+  d = { "<cmd>lua require('persistence').stop()<cr>", "stop saving" },
+  l = { "<cmd>lua require('persistance').load()<cr>", "restore the session for the current directory" },
+  r = { "<cmd>lua require('persistence').load({ last = true })<cr>)", "restore the last session" },
+  s = { "<cmd>lua require('persistence').save()<cr>", "stop saving" },
+}
 
 lvim.leader = "space"
 lvim.keys.insert_mode["<C-s>"] = "<cmd>w<cr>"
@@ -70,6 +75,7 @@ lvim.keys.normal_mode["gL"] = ":BufferNext<cr>"
 lvim.keys.normal_mode["gH"] = ":BufferPrev<cr>"
 lvim.keys.normal_mode["<space><space>"] = "<cmd>BufferNext<cr>"
 
+vim.cmd [[ nnoremap <F2> :%s/<c-r><c-w>/<c-r><c-w>/gc<c-f>$F/i ]]
 -- Allow pasting same thing many times
 lvim.keys.visual_mode["p"] = '""p:let @"=@0<CR>'
 lvim.keys.visual_block_mode["p"] = '""p:let @"=@0<CR>'
@@ -118,6 +124,11 @@ lvim.plugins = {
       require("octo").setup()
     end,
   },
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    config = [[require('user.core.sessions').setup()]],
+  },
 }
 
 -- generic LSP settings
@@ -147,7 +158,13 @@ lvim.keys.normal_mode["<c-e>"] = ":lua package.loaded['scratch'] = nil; require(
 
 vim.cmd [[command! -nargs=1 GS :lua require("telescope.builtin").grep_string(require("telescope.themes").get_dropdown({ prompt_title = "Fuzzy grep string " .. "<args>", search = "<args>" }))]]
 
-lvim.builtin.which_key.mappings["Li"] = {
-  "<cmd>lua package.loaded['core.info']=nil; require('core.info').toggle_popup(vim.bo.filetype)<cr>",
+lvim.builtin.which_key.mappings["Lt"] = {
+  "<cmd>lua package.loaded['core.info']=nil; package.loaded['interface.popup']=nil; require('core.info').toggle_popup(vim.bo.filetype)<cr>",
   "Toggle LunarVim Info",
 }
+
+function _G.dump(...)
+  local objects = vim.tbl_map(vim.inspect, { ... })
+  print(unpack(objects))
+  return ...
+end
