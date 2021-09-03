d9c81d7742dd1ee229cd66af21a4a78dadf5dbe0
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-09-03 17:43:00 +0200

    LSP: update handlers

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -37,21 +37,26 @@ vim.lsp.protocol.CompletionItemKind = {
 }
 
 local function setup_lsp_keybindings(bufnr)
-  local wk = require "which-key"
-  local keys = {
-    ["K"] = { "<cmd>lua vim.lsp.buf.hover({focusable = false})<CR>", "Show hover" },
-    ["gd"] = { "<cmd>lua vim.lsp.buf.definition()<CR>", "Goto Definition" },
-    ["gD"] = { "<cmd>lua vim.lsp.buf.declaration()<CR>", "Goto declaration" },
-    ["gr"] = { "<cmd>lua vim.lsp.buf.references()<CR>", "Goto references" },
-    ["gi"] = { "<cmd>lua vim.lsp.buf.implementation()<CR>", "Goto implementation" },
-    ["gs"] = { "<cmd>lua vim.lsp.buf.signature_help({focusable = false})<CR>", "show signature help" },
-    ["gp"] = { "<cmd>lua require'lsp.peek'.Peek('definition')<CR>", "Peek definition" },
-    ["gl"] = { "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", "Show line diagnostics" },
-  }
-  wk.register(keys, { mode = "n", buffer = bufnr })
+  local status_ok, wk = pcall(require, "which-key")
+  if not status_ok then
+    return
+  end
   local visual_keys = { f = { "<cmd>lua vim.lsp.buf.formatting()<cr>", "Format" } }
 
   wk.register(visual_keys, { mode = "v", buffer = bufnr })
+
+  local keys = {
+    ["K"] = { "<cmd>lua vim.lsp.buf.hover()<CR>", "Show hover" },
+    ["gd"] = { "<cmd>lua vim.lsp.buf.definition()<CR>", "Goto Definition" },
+    ["gD"] = { "<cmd>lua vim.lsp.buf.declaration()<CR>", "Goto declaration" },
+    ["gr"] = { "<cmd>lua vim.lsp.buf.references()<CR>", "Goto references" },
+    ["gI"] = { "<cmd>lua vim.lsp.buf.implementation()<CR>", "Goto Implementation" },
+    ["gs"] = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "show signature help" },
+    ["gp"] = { "<cmd>lua require'lsp.peek'.Peek('definition')<CR>", "Peek definition" },
+    ["gl"] = { "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", "Show line diagnostics" },
+  }
+
+  wk.register(keys, { mode = "n", buffer = bufnr })
 end
 
 function M.get_ls_capabilities(client_id)
@@ -68,18 +73,26 @@ function M.get_ls_capabilities(client_id)
 
   print("client id: ", client.id)
   print("client name:", client.name)
-  -- print(vim.inspect(client))
   print("resolved_capabilities:", vim.inspect(lsp_caps))
 
   return lsp_caps
 end
 
 local function setup_document_highlight(client)
+  -- TODO: move this so that it done only once
+  -- https://gist.github.com/romainl/379904f91fa40533175dfaec4c833f2f
+  vim.cmd [[ hi LspReferenceRead cterm=bold ctermbg=red guibg=#464646 ]]
+  vim.cmd [[ hi LspReferenceText cterm=bold ctermbg=red guibg=#464646 ]]
+  vim.cmd [[ hi LspReferenceWrite cterm=bold ctermbg=red guibg=#464646 ]]
   -- Set autocommands conditional on server_capabilities
   if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_command [[autocmd CursorHold  <buffer> lua vim.lsp.buf.document_highlight()]]
-    vim.api.nvim_command [[autocmd CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()]]
-    vim.api.nvim_command [[autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()]]
+    vim.cmd [[
+      augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold  <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END 
+    ]]
   end
 end
 
@@ -88,15 +101,10 @@ function M.common_on_init(client, bufnr)
   vim.lsp.handlers["textDocument/publishDiagnostics"] = handlers.set_diagnostics
   vim.lsp.handlers["textDocument/hover"] = handlers.set_hover
   vim.lsp.handlers["textDocument/signatureHelp"] = handlers.set_sigature
-  -- TODO: move this so that it done only once
-  -- https://gist.github.com/romainl/379904f91fa40533175dfaec4c833f2f
-  vim.cmd [[ hi LspReferenceRead cterm=bold ctermbg=red guibg=#464646 ]]
-  vim.cmd [[ hi LspReferenceText cterm=bold ctermbg=red guibg=#464646 ]]
-  vim.cmd [[ hi LspReferenceWrite cterm=bold ctermbg=red guibg=#464646 ]]
+  setup_document_highlight(client)
 end
 
 function M.common_on_attach(client, bufnr)
-  setup_document_highlight(client)
   setup_lsp_keybindings(bufnr)
 end
 
@@ -107,7 +115,7 @@ function M.setup()
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
-  local opts = {
+  local default_config = {
     autostart = true,
     on_attach = M.common_on_attach,
     on_init = M.common_on_init,
@@ -118,12 +126,14 @@ function M.setup()
   }
 
   for _, server in ipairs(servers) do
-    local status_ok, provider_opts = pcall(require, "lsp/providers/" .. server)
+    local status_ok, config = pcall(require, "lsp/providers/" .. server)
+    local new_config
     if status_ok then
-      local new_config = vim.tbl_deep_extend("keep", {}, opts, provider_opts)
+      new_config = vim.tbl_deep_extend("keep", vim.empty_dict(), config)
+      new_config = vim.tbl_deep_extend("keep", new_config, default_config)
       nvim_lsp[server].setup(new_config)
     else
-      nvim_lsp[server].setup(opts)
+      nvim_lsp[server].setup(default_config)
     end
   end
 
