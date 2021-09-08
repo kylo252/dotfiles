79fe7054e793d0042a674acacd46d4167d59c7b6
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-09-08 08:51:52 +0200

    yamlls updates

--- .config/nvim/lua/core/lightspeed.lua
+++ .config/nvim/lua/core/lightspeed.lua
@@ -1,7 +1,7 @@
 require("lightspeed").setup {
-  jump_to_first_match = true,
+  jump_to_first_match = false,
   jump_on_partial_input_safety_timeout = 400,
-  highlight_unique_chars = false,
+  highlight_unique_chars = true,
   grey_out_search_area = true,
   match_only_the_start_of_same_char_seqs = true,
   limit_ft_matches = 5,
--- .config/nvim/lua/lsp/providers/yamlls.lua
+++ .config/nvim/lua/lsp/providers/yamlls.lua
@@ -2,10 +2,21 @@ return {
   -- filetypes = {"yaml"},
   settings = {
     yaml = {
-      scheme = {
+      format = {
+            enable = true
+      },
+      validate = true,
+      schemas = {
         ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose.yml",
         ["https://json.schemastore.org/pre-commit-config.json"] = ".pre-commit-config.yaml",
+        kubernetes = "/*.k8s.yaml"
       },
+      -- scheme = {
+      --   ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose.yml",
+      --   ["https://json.schemastore.org/pre-commit-config.json"] = ".pre-commit-config.yaml",
+      --   ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.20.5-standalone-strict/all.json"] = "frontend-service.yaml"
+      -- },
+      schemaDownload = {  enable = true }
     },
   },
 }
