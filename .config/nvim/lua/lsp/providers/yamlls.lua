4cc897a44e9446ab5425f1f50178bdf675627309
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-10-06 08:21:44 +0200

    chore: formatting

--- .config/nvim/lua/lsp/providers/yamlls.lua
+++ .config/nvim/lua/lsp/providers/yamlls.lua
@@ -3,20 +3,23 @@ return {
   settings = {
     yaml = {
       format = {
-            enable = true
+        enable = true,
       },
       validate = true,
       schemas = {
         ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose.yml",
         ["https://json.schemastore.org/pre-commit-config.json"] = ".pre-commit-config.yaml",
-        kubernetes = "/*.k8s.yaml"
+        kubernetes = "/*.k8s.yaml",
       },
       -- scheme = {
       --   ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose.yml",
       --   ["https://json.schemastore.org/pre-commit-config.json"] = ".pre-commit-config.yaml",
       --   ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.20.5-standalone-strict/all.json"] = "frontend-service.yaml"
       -- },
-      schemaDownload = {  enable = true }
+      schemaDownload = { enable = true },
+      trace = {
+        server = "verbose",
+      },
     },
   },
 }
