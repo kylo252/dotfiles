a01503285612fe916807af7f8dc1bc536f9096b6
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-10-24 13:55:50 +0200

    nvim: update indent-line settings

--- .config/nvim/lua/core/indent.lua
+++ .config/nvim/lua/core/indent.lua
@@ -6,9 +6,9 @@ vim.g.indent_blankline_filetype_exclude = {
   "terminal",
   "dashboard",
   "lspinfo",
-  "lsp-installer"
+  "lsp-installer",
 }
 vim.g.indent_blankline_buftype_exclude = { "terminal" }
 
 vim.g.indent_blankline_show_trailing_blankline_indent = false
-vim.g.indent_blankline_show_first_indent_level = true
+vim.g.indent_blankline_show_first_indent_level = false
