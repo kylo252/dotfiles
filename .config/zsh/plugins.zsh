f4998e83485dd5cc1558f8c9ba4d00c145ab8dd8
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-11-05 11:13:28 +0100

    zsh: update fsh remote

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -10,18 +10,11 @@ LFCD="$GOPATH/src/github.com/gokcehan/lf/etc/lfcd.sh"
 
 source "$XDG_DATA_HOME/zsh/plugins/znap/znap.zsh"
 
-znap source kazhala/dotbare
-
 znap source romkatv/powerlevel10k
-
-znap source zdharma/fast-syntax-highlighting
-
+znap source zdharma-continuum/fast-syntax-highlighting
 znap source zsh-users/zsh-autosuggestions
-
 znap source zsh-users/zsh-completions
-
 znap source Aloxaf/fzf-tab
-
 znap source reegnz/jq-zsh-plugin
 
 # znap eval pip-completion "pip completion --zsh  # $PYENV_VERSION"
@@ -43,6 +36,8 @@ source <(fnm env --fnm-dir="$XDG_DATA_HOME/fnm" --shell=zsh)
 
 autoload -Uz kp
 
+# turn off git maintenance
+zstyle ':znap:*:<glob pattern>' git-maintenance off
 # disable sort when completing `git checkout`
 zstyle ':completion:*:git-checkout:*' sort false
 # set descriptions format to enable group support
