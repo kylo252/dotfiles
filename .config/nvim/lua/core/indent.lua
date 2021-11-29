4ca86c7c58bb9113a54e63201b21a1f400bf882c
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-11-29 10:34:07 +0100

    nvim: test treesitter indent

--- .config/nvim/lua/core/indent.lua
+++ .config/nvim/lua/core/indent.lua
@@ -15,7 +15,7 @@ function M.setup()
 
     show_trailing_blankline_indent = false,
     show_first_indent_level = false,
-    use_treesitter = false,
+    use_treesitter = true,
   }
 
   require("indent_blankline").setup(opts)
