b6cf3c7f214fb5874f96c22eab2e4058b687b123
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-12-04 17:46:19 +0100

    nvim: update nvim-tree settings

--- .config/nvim/lua/core/nvimtree.lua
+++ .config/nvim/lua/core/nvimtree.lua
@@ -8,14 +8,11 @@ local opts = {
     folder_arrows = 1,
     tree_width = 35,
   },
-  ignore = { ".git", "node_modules", ".cache" },
   quit_on_open = 0,
   respect_buf_cwd = 1,
-  hide_dotfiles = 0,
   git_hl = 1,
+  disable_window_picker = 0,
   root_folder_modifier = ":t",
-  allow_resize = 1,
-  auto_ignore_ft = { "startify", "dashboard" },
 }
 
 function M.setup()
@@ -34,29 +31,62 @@ function M.setup()
   local tree_cb = nvim_tree_config.nvim_tree_callback
 
   local setup_opts = {
-    auto_open = false,
+    disable_netrw = true,
+    hijack_netrw = true,
+    open_on_setup = false,
+    ignore_ft_on_setup = {
+      "startify",
+      "dashboard",
+      "alpha",
+    },
+    update_to_buf_dir = {
+      enable = true,
+      auto_open = true,
+    },
     auto_close = true,
-    tab_open = false,
+    open_on_tab = false,
+    hijack_cursor = false,
     update_cwd = true,
-    disable_netrw = false,
-    hijack_netrw = false,
+    diagnostics = nil,
     update_focused_file = {
       enable = true,
       update_cwd = true,
+      ignore_list = {},
     },
-    diagnostics = nil,
+    system_open = {
+      cmd = nil,
+      args = {},
+    },
+    git = {
+      enable = true,
+      ignore = false,
+      timeout = 200,
+    },
     view = {
       width = 30,
+      height = 30,
       side = "left",
-      auto_resize = false,
+      auto_resize = true,
+      number = false,
+      relativenumber = false,
       mappings = {
         custom_only = false,
         list = {
           { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
           { key = "h", cb = tree_cb "close_node" },
+          { key = "h", cb = tree_cb "close_node" },
+          { key = "C", cb = tree_cb "cd" },
+          { key = "gtf", cb = "<cmd>lua require'core.nvimtree'.start_telescope('find_files')<cr>" },
+          { key = "gtg", cb = "<cmd>lua require'core.nvimtree'.start_telescope('live_grep')<cr>" },
         },
       },
     },
+    filters = {
+      dotfiles = false,
+      custom = { ".git", "node_modules", ".cache" },
+    },
+    auto_open = false,
+    tab_open = false,
   }
 
   -- Add nvim_tree open callback
@@ -91,6 +121,16 @@ function M.change_tree_dir(dir)
   if lib_status_ok then
     lib.change_dir(dir)
   end
+end
+
+function M.start_telescope(telescope_mode)
+  local node = require("nvim-tree.lib").get_node_at_cursor()
+  local abspath = node.link_to or node.absolute_path
+  local is_folder = node.open ~= nil
+  local basedir = is_folder and abspath or vim.fn.fnamemodify(abspath, ":h")
+  require("telescope.builtin")[telescope_mode] {
+    cwd = basedir,
+  }
 end
 
 return M
