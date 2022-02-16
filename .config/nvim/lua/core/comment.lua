682e78d109117362f104649b21a7a4adf39b8f2c
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-02-16 10:26:28 +0100

    nvim(neogen): update template

--- .config/nvim/lua/core/comment.lua
+++ .config/nvim/lua/core/comment.lua
@@ -4,5 +4,10 @@ function M.setup()
   require("kommentary.config").configure_language("lua", {
     prefer_single_line_comments = true,
   })
+  require("kommentary.config").configure_language("c", {
+    prefer_single_line_comments = true,
+    single_line_comment_string = "///",
+    multi_line_comment_strings = { "/*", "*/" },
+  })
 end
 return M
--- .config/nvim/lua/core/neogen.lua
+++ .config/nvim/lua/core/neogen.lua
@@ -3,20 +3,20 @@ local M = {}
 function M.setup()
   local i = require("neogen.types.template").item
   local custom_template = {
-    { nil, "/// ", { no_results = true, type = { "func", "file", "class" } } },
+    -- { nil, "/// ", { no_results = true, type = { "func", "file", "class" } } },
     { nil, "/// @file", { no_results = true, type = { "file" } } },
     { nil, "/// @brief $1", { no_results = true, type = { "func", "file", "class" } } },
-    { nil, "/// ", { no_results = true, type = { "func", "file", "class" } } },
+    -- { nil, "/// ", { no_results = true, type = { "func", "file", "class" } } },
     { nil, "", { no_results = true, type = { "file" } } },
 
-    { nil, "/// ", { type = { "func", "class" } } },
+    -- { nil, "/// ", { type = { "func", "class" } } },
     { i.ClassName, "/// @class %s", { type = { "class" } } },
     { nil, "/// @brief $1", { type = { "func", "class" } } },
-    { nil, "/// ", { type = { "func", "class" } } },
+    -- { nil, "/// ", { type = { "func", "class" } } },
     { i.Tparam, "/// @tparam %s $1" },
     { i.Parameter, "/// @param %s $1" },
     { i.Return, "/// @return $1" },
-    { nil, "/// ", { type = { "func", "class" } } },
+    -- { nil, "/// ", { type = { "func", "class" } } },
   }
 
   require("neogen").setup {
