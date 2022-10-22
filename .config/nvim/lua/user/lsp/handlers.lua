adf1dfa871458618dfefb99e912ba7b388381ae8
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-10-22 16:08:56 +0200

    perf(nvim): fix diagnostic signs

--- .config/nvim/lua/user/lsp/handlers.lua
+++ .config/nvim/lua/user/lsp/handlers.lua
@@ -45,6 +45,37 @@ function M.setup()
   vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, float_opts)
   vim.lsp.handlers["textDocument/signatureHelp"] =
     vim.lsp.with(vim.lsp.handlers.signature_help, float_opts)
+
+  -- Get a reference to the original signs handler
+  local orig_signs_handler = vim.diagnostic.handlers.signs
+  vim.diagnostic.handlers.signs = {
+    show = function(ns, bufnr, _, opts)
+      -- diagnostics from the whole buffer, not just those passed to the handler
+      local diagnostics = vim.diagnostic.get(bufnr)
+
+      -- perf: avoid showing too many signs
+      if #diagnostics > 20 then
+        return
+      end
+
+      -- Find the "worst" diagnostic per line
+      local max_severity_per_line = {}
+      for _, d in pairs(diagnostics) do
+        local m = max_severity_per_line[d.lnum]
+        if not m or d.severity < m.severity then
+          max_severity_per_line[d.lnum] = d
+        end
+      end
+
+      -- Pass the filtered diagnostics (with our custom namespace) to
+      -- the original handler
+      local filtered_diagnostics = vim.tbl_values(max_severity_per_line)
+      orig_signs_handler.show(ns, bufnr, filtered_diagnostics, opts)
+    end,
+    hide = function(ns, bufnr)
+      orig_signs_handler.hide(ns, bufnr)
+    end,
+  }
 end
 
 return M
