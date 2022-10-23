7b9b2f9f5824daaf849d3a6c4429336a1ab78833
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-10-23 10:19:34 +0200

    zsh: update extract function

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,24 +1,24 @@
 #!/usr/bin/env zsh
 
-# ChrisTitusTech/zsh/blob/master/aliasrc
-# ex - archive extractor
-# usage: ex <file>
 function ex() {
   if [ -f "$1" ]; then
     case $1 in
-      *.tar.bz2) tar xjf "$1" ;;
-      *.tar.gz) tar xzf "$1" ;;
-      *.tar.xz) tar xJf "$1" ;;
-      *.bz2) bunzip2 "$1" ;;
-      *.rar) unrar x "$1" ;;
-      *.gz) gunzip "$1" ;;
       *.tar) tar xf "$1" ;;
-      *.txz) tar Jxvf "$1" ;;
-      *.tbz2) tar xjf "$1" ;;
-      *.tgz) tar xzf "$1" ;;
-      *.zip) unzip "$1" ;;
-      *.Z) uncompress "$1" ;;
+      *.tar.bz2 | *.tbz | *.tbz2) tar xjf "$1" ;;
+      *.tar.gz | *.tgz) tar xzf "$1" ;;
+      *.tar.lz4 ) lz4 -c -d "$1" | tar xvf - ;;
+      *.tar.xz | *.txz) tar --xz -xf "$1" ;;
+      *.tar.zma ) tar --lzma -xf "$1" ;;
+      *.tar.zst | *.tzst) tar --zstd -xf "$1" ;;
       *.7z) 7z x "$1" ;;
+      *.bz2) bunzip2 "$1" ;;
+      *.gz) gunzip "$1" ;;
+      *.lz4) lz4 -d "$1" ;;
+      *.lzma) unlzma "$1" ;;
+      *.rar) unrar x "$1" ;;
+      *.zip) unzip "$1" ;;
+      *.zst) unzstd "$1" ;;
+      *.Z) uncompress "$1" ;;
       *) echo "'$1' cannot be extracted via ex()" ;;
     esac
   else
