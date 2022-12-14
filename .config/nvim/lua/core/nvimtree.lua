64b744382c92c9d29896d4af9860410f9b9f870a
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-12-14 11:46:03 +0100

    wm: updates

--- .config/nvim/lua/core/nvimtree.lua
+++ .config/nvim/lua/core/nvimtree.lua
@@ -26,7 +26,6 @@ function M.setup()
     open_on_setup = false,
     open_on_setup_file = false,
     open_on_tab = false,
-    focus_empty_on_setup = false,
     ignore_buf_on_tab_change = {},
     sort_by = "name",
     root_dirs = {},
