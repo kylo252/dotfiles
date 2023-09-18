3fc5eb8e95659fa0c0e23a5bf2131533662f54fb
kylo252 <59826753+kylo252@users.noreply.github.com>
2023-09-18 10:47:40 +0200

    nvim: remove deprecated setting

--- .config/nvim/lua/core/nvimtree.lua
+++ .config/nvim/lua/core/nvimtree.lua
@@ -59,7 +59,6 @@ function M.setup()
     sync_root_with_cwd = true,
     reload_on_bufenter = false,
     respect_buf_cwd = true,
-    remove_keymaps = false,
     select_prompts = false,
     update_focused_file = {
       enable = true,
